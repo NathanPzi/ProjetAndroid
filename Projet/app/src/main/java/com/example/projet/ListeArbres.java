133eefaf47a5567b60e9f2fa2dc49446a162c4db
NathanPzi <piazzi.nathan@gmail.com>
2022-02-21 09:20:36 +0100

    jj

---
+++ Projet/app/src/main/java/com/example/projet/ListeArbres.java
@@ -0,0 +1,14 @@
+package com.example.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListeArbres extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_arbres);
+    }
+}
---
+++ Projet/app/src/main/java/com/example/projet/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflaterMenu = getMenuInflater();
+        inflaterMenu.inflate(R.menu.menuaccueil, menu);
+        return true;
+    }
+}
